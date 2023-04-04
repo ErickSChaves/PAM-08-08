5941e0d0aa852f1741414034fe63bc9c40ff3704
Your Name <you@example.com>
2023-04-04 12:14:08 -0300

    first commit

---
+++ src/data/movies.js
@@ -0,0 +1,97 @@
+const DATA =
+[
+    {
+
+        Id:'01',
+        Nome:'Creed III',
+        Nota:'9.3',
+        Imagem:'Creed.jpg'
+
+    },
+
+    {
+
+        Id:'02',
+        Nome:'Pânico 6',
+        Nota:'9.0',
+        Imagem:'panico.jpg'
+    
+        },
+
+    {
+
+         Id:'03',
+        Nome:'John Wick 4',
+         Nota:'9.6',
+        Imagem:'john.jpg'
+        
+     },
+
+
+     {
+
+         Id:'04',
+         Nome:'Primeira Comunhão',
+         Nota:'7.4',
+         Imagem:'comunhao.jpg'
+            
+    },
+
+     {
+
+        Id:'05',
+        Nome:'M3GAN',
+        Nota:'8.4',
+        Imagem:'megan.jpg'
+                
+    },
+
+
+    {
+
+        Id:'06',
+        Nome:'Homem-Formiga e a Vespa',
+        Nota:'8.2',
+        Imagem:'homem.jpg'
+                    
+    },
+
+
+    {
+
+        Id:'07',
+        Nome:'Marlowe',
+        Nota:'5.5',
+        Imagem:'Marlowe.jpg'
+                        
+    },
+
+    {
+
+        Id:'08',
+        Nome:'Super Mario Bros',
+        Nota:'9.5',
+        Imagem:'mario.jpg'
+                            
+    },
+
+
+    {
+
+        Id:'09',
+        Nome:'Dungeons & Dragons',
+        Nota:'9.5',
+        Imagem:'dun.jpg'
+                                
+    },
+
+
+    {
+
+        Id:'10',
+        Nome:'Shazam! Fúria dos Deuses',
+        Nota:'8.9',
+        Imagem:'sha.jpg'
+                                    
+    },
+]
